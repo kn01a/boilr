851e9bcfd49176eabc75314287eb44feba1403fd
kn01a <70111023+kn01a@users.noreply.github.com>
2023-10-20 14:09:10 +0200

    Update app.py

--- boilr/app.py
+++ boilr/app.py
@@ -179,7 +179,7 @@ def run():
                     logger.info("Status: %s", "active" if boilr.status[0] else "inactive")
                     boilr.status_prev = boilr.status
 
-                    if(config.HueConfig.hue_active):
+                    if config.HueConfig.hue_active:
                         hue.switch(boilr.status[0])
 
                     if not rpi_gpio.output_relay(config.RpiConfig.rpi_channel_relay_out, boilr.status[0]):
@@ -212,9 +212,9 @@ def manual_override(args):
             logger.debug("Manual override: contactor %s", "closed" if args == 1 else "open")
             logger.info("Status: %s (manual)", "active" if args == 1 else "inactive")
             
-            if(config.HueConfig.hue_active):
+            if config.HueConfig.hue_active:
                 hue.switch(True if args == 1 else False)
-                
+
             
             if not rpi_gpio.output_relay(config.RpiConfig.rpi_channel_relay_out, True if args == 1 else False):
                 raise SystemError("GPIO channel failed")
